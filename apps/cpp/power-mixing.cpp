fb1849611cf7d5a5cc58ad710cf89b4aed3a3418
Samarth Kulshreshtha <smkuls@gmail.com>
2018-05-02 23:14:48 -0500

    Implementing power mixing in C++

---
+++ apps/cpp/power-mixing.cpp
@@ -0,0 +1,99 @@
+#include <NTL/ZZ_pXFactoring.h>
+#include <cassert>
+#include <stdio.h>
+
+using namespace std;
+using namespace NTL;
+
+void print_vec(const Vec<ZZ_p> &data)
+{
+    for (auto i : data)
+    {
+        cout << i << endl;
+    }
+}
+
+Vec<ZZ_p> compute_powers(const ZZ_p &a, const unsigned int &k, const Vec<ZZ_p> &bs)
+{
+    assert (bs.length() == k);
+
+    ZZ_p b = bs[0];
+
+    Vec<ZZ_p> apows;
+    apows.SetLength(k+1);
+    apows[0] = a;
+
+    Vec<ZZ_p> DiagMinus1;
+    DiagMinus1.append(ZZ_p(1));
+
+    for (unsigned int m = 1; m <= k; m++)
+    {
+        Vec<ZZ_p> DiagM;
+        DiagM.SetLength(m+1);
+        DiagM[0]  = bs[m-1];
+        ZZ_p sum(0);
+        for (unsigned int i = 1; i <= m; i++)
+        {
+            sum += DiagMinus1[i-1];
+            DiagM[i] = (a-b) * sum + bs[m-1];
+        }
+
+        apows[m] = DiagM[m];
+        DiagMinus1 = DiagM;
+    }
+
+    Vec<ZZ_p> result;
+    result.SetLength(k);
+    for (int i = 1; i <= k; i++)
+    {
+        result[i-1] = apows[i];
+    }
+
+    return result;
+}
+
+Vec<ZZ_p> get_k_powers_of_b(const ZZ_p &b, const unsigned int &k)
+{
+    Vec<ZZ_p> bs;
+    bs.SetLength(k);
+    bs[0] = b;
+    for (unsigned int i = 1; i < k; i++)
+    {
+        bs[i] = bs[i-1]*b;
+    }
+    return bs;
+}
+
+/**
+ * Input:
+ * field_modulus: Modulus of the field.
+ * a : Number whose powers need to be computed.
+ * k : Number of powers to be computed.
+ * bs : Pre computed powers of some random number.
+ * */
+int main()
+{
+    long field_modulus;
+    cin >> field_modulus;
+
+    // Initialize the field with the modulus.
+    ZZ_p::init(ZZ(field_modulus));
+
+    ZZ_p a;
+    cin >> a;
+
+    unsigned int k;
+    cin >> k;
+
+    Vec<ZZ_p> bs;
+    bs.SetLength(k);
+    for (int i = 0; i < k; i++)
+    {
+        cin >> bs[i];
+    }
+
+    auto apows = compute_powers(a, k, bs);
+    print_vec(apows);
+
+    return 0;
+}
