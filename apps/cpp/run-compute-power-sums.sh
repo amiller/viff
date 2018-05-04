46d783cf7b3b8856f65478b6ebfd7bb6b1b3a69e
Samarth Kulshreshtha <smkuls@gmail.com>
2018-05-04 15:42:10 -0500

    Moving random inputs to a separate folder

--- apps/cpp/run-compute-power-sums.sh
+++ apps/cpp/run-compute-power-sums.sh
@@ -2,5 +2,5 @@ make clean;
 make;
 k=$1;
 n=$(lscpu -p | grep -c "^[0-9]");
-mkdir -p random$k; seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums $k {} > random$k/random{}.input"
-rm -f powers.sum; time seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums random$k/random{}.input"
+mkdir -p inputs/random$k; seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums $k {} > inputs/random$k/random{}.input"
+rm -f powers.sum; time seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums inputs/random$k/random{}.input"
