9e2e6e2dd51c03998585c2090ec4ce387386fae5
Donghang Lu <lu562@purdue.edu>
2018-05-06 20:58:12 -0400

    add "how to run" section in readme

--- apps/powermix-online/runlocal.sh
+++ apps/powermix-online/runlocal.sh
@@ -2,7 +2,7 @@
 n=3
 k=32
 fname="powermixing_online_phase3.py"
-
+python generate-config-files.py -n 3 -t 1 localhost:9001 localhost:9002 localhost:9003
 for c in `seq 1 $n`
 do  
     gnome-terminal -e "sh testlocal.sh $k $c"
