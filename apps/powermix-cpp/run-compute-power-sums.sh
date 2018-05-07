8b224b91293ebfb6461e31605317ece016e59851
Donghang Lu <lu562@purdue.edu>
2018-05-06 20:50:58 -0400

    remove redundancy

--- apps/powermix-cpp/run-compute-power-sums.sh
+++
@@ -1,6 +0,0 @@
-make clean;
-make;
-k=$1;
-n=$(lscpu -p | grep -c "^[0-9]");
-mkdir -p inputs/random$k; seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums $k {} > inputs/random$k/random{}.input"
-rm -f powers.sum; time seq $k | xargs -n1 -P$n -I{} sh -c "./compute-power-sums inputs/random$k/random{}.input"
--- apps/powermix-online/solver_phase4/solver/test.py
+++
@@ -1,6 +0,0 @@
-import solver
-k = 999
-dc_sums = [k for i in range(k)]
-print(solver.solve(dc_sums))
-
-
