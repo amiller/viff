e7899269f50e9d309b48c3fe6a693373c44febb1
Donghang Lu <lu562@purdue.edu>
2018-05-07 21:52:39 -0400

    fix script

--- apps/powermix-online/testlocal.sh
+++ apps/powermix-online/testlocal.sh
@@ -9,7 +9,7 @@ mkdir phase2-party$n
 cp -r cpp_phase2 phase2-party$n/cpp_phase2
 cd phase2-party$n/cpp_phase2
 sh run-compute-power-sums-local.sh $k $n
-cp powers.sum$n ../../powers.sum$n
+cp powers.sum ../../powers.sum$n
 cd ../..
 python powermixing_online_phase3.py --no-ssl player-$n.ini $k
 cp party$n-powermixing-online-phase3-output solver_phase4/party$n-powermixing-online-phase3-output
