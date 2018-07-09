524713c0cb877c62bd7dd6afd044b0d1c552c381
Donghang Lu <lu562@purdue.edu>
2018-07-09 15:59:55 -0400

    comment error info to make test faster

--- apps/powermix-online/powermixing_online_phase1.py
+++ apps/powermix-online/powermixing_online_phase1.py
@@ -111,8 +111,8 @@ class OnlineProtocol:
 	if int(line) != p:
 		print "p dismatch!! p in file is %d"%(int(line))
 	line = FD.readline()
-	if int(line) != k:
-		print "k dismatch!! k in file is %d"%(int(line))
+	#if int(line) != k:
+	#	print "k dismatch!! k in file is %d"%(int(line))
 	self.Zp = GF(p)
 
 	line = FD.readline()
