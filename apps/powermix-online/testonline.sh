73dd7004af90fad793c604b2a6869282e28ecbbd
Donghang Lu <lu562@purdue.edu>
2018-07-09 13:11:12 -0400

    fix

--- apps/powermix-online/testonline.sh
+++ apps/powermix-online/testonline.sh
@@ -2,7 +2,7 @@
 k=$1
 n=$2
 cd ~/viff/apps/powermix-online/
-start_tm=`date +%s%N`；
+start_tm=`date +%s%N`;
 python powermixing_online_phase1.py --no-ssl player-$n.ini $k
 cd cpp_phase2
 sh run-compute-power-sums-online.sh $k $n
@@ -12,7 +12,7 @@ python powermixing_online_phase3.py --no-ssl player-$n.ini $k
 cp party$n-powermixing-online-phase3-output solver_phase4/party$n-powermixing-online-phase3-output
 cd solver_phase4
 python3 solver.py $n
-end_tm=`date +%s%N`；
+end_tm=`date +%s%N`;
 use_tm=`echo $end_tm $start_tm | awk '{ print ($1 - $2) / 1000000000}'`
 echo $use_tm
 
