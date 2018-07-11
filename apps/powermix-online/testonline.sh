50c8e97c997118d19a2f5aa914adde71ddfb4992
Donghang Lu <lu562@purdue.edu>
2018-07-11 15:45:21 -0400

    remove compile commands

--- apps/powermix-online/testonline.sh
+++ apps/powermix-online/testonline.sh
@@ -2,10 +2,10 @@
 k=$1
 n=$2
 cd ~/viff/apps/powermix-online/
-cd cpp_phase2
-make clean 
-make
-cd ..
+#cd cpp_phase2
+#make clean 
+#make
+#cd ..
 
 start_tm=`date +%s%N`;
 python powermixing_online_phase1.py --no-ssl player-$n.ini $k
