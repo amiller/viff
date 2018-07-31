39a8f88041a70f954f91433393037b02844a6dd9
Donghang Lu <lu562@purdue.edu>
2018-07-31 15:37:37 -0400

    update

--- apps/powermix-online/generate_input.sh
+++ apps/powermix-online/generate_input.sh
@@ -2,11 +2,8 @@
 k=$1
 n=$2
 cd ~/viff/apps/powermix-online/
-#cd cpp_phase2
-#make clean 
-#make
-#cd ..
 
+export PYTHONPATH=$PYTHONPATH:$HOME/opt/lib/python
 
 python preinput_generator.py --no-ssl player-$n.ini $k
 python input_generator.py --no-ssl player-$n.ini $k
