86631b8ac73bf3761ea9e36f58667ff332fa63e3
Donghang Lu <lu562@purdue.edu>
2018-08-15 14:15:55 -0400

    f

--- apps/powermix-batch/butterfly_offline.py
+++ apps/powermix-batch/butterfly_offline.py
@@ -69,93 +69,92 @@ def record_stop():
 
 	stop = time.time()
 	print "Total time used: %.3f sec" % (stop-start)
-    '''
+	'''
     if runtime.id == 1:
         f = open('time.txt', 'w')
         f.write(stop-start)
         f.close()
     '''
 	print "*" * 64
-    #return x
+
 
 
 
 
 class OfflineProtocol:
-    # this is only for testing generating random -1/1 shares.
-    def __init__(self, runtime,k):
-	self.k =k
-	self.runtime = runtime
-	self.ramdom_shares = [0 for i in range(self.k * int(math.log(self.k,2)))]
-	self.p = find_prime(2**256, blum=True)
-	self.Zp = GF(self.p)
-	self.triggers = [Share(self.runtime,self.Zp) for i in range(self.k * int(math.log(self.k,2)))]
-	
-	#print self.p
 
-	n = self.runtime.num_players
-	t = self.runtime.threshold
-        T = n - 2*t
-
-	record_start()
-	for i in range(self.k * int(math.log(self.k,2))):
-		r = self.runtime.single_share_random(T,t,GF(self.p))
-
-		def random_ready(r,cnt):
-			
-			u = r[0] * r[0]
-			open_u = self.runtime.open(u)
-
-			open_u.addCallback(self.calculate_share,r[0],cnt)
-	        self.runtime.schedule_callback(r, random_ready,i)
-
-	list = [self.triggers[i] for i in range(self.k * int(math.log(self.k,2)))]
-	result = gather_shares(list)
-	result.addCallback(self.preprocess_ready)
+	def __init__(self, runtime,k):
+		self.k =k
+		self.runtime = runtime
+		self.ramdom_shares = [0 for i in range(self.k * int(math.log(self.k,2)))]
+		self.p = find_prime(2**256, blum=True)
+		self.Zp = GF(self.p)
+		self.triggers = [Share(self.runtime,self.Zp) for i in range(self.k * int(math.log(self.k,2)))]
 
 
-    def calculate_share(self,result,r,i):
-	#print ""caculating shares
-	v = result**((-(self.p+1)/4)%(self.p - 1))
-	self.ramdom_shares[i] = r * v
-	self.triggers[i].callback(1)
-	
-    def preprocess_ready(self,result):
-	print "preprocess_ready"
-	record_stop()
-	self.write_to_file(self.ramdom_shares)
-	record_stop()
-        results = self.runtime.synchronize()
-        self.runtime.schedule_callback(results, lambda _: self.runtime.shutdown())
+		n = self.runtime.num_players
+		t = self.runtime.threshold
+		T = n - 2*t
 
-	'''
-	for i in range(self.k * int(math.log(self.k,2))): 
-		open_1= self.runtime.open(self.ramdom_shares[i])
+		record_start()
+		for i in range(self.k * int(math.log(self.k,2))):
+			r = self.runtime.single_share_random(T,t,GF(self.p))
+
+			def random_ready(r,cnt):
+				
+				u = r[0] * r[0]
+				open_u = self.runtime.open(u)
+
+				open_u.addCallback(self.calculate_share,r[0],cnt)
+				self.runtime.schedule_callback(r, random_ready,i)
+
+		list = [self.triggers[i] for i in range(self.k * int(math.log(self.k,2)))]
+		result = gather_shares(list)
+		result.addCallback(self.preprocess_ready)
+
+
+	def calculate_share(self,result,r,i):
+		#print ""caculating shares
+		v = result**((-(self.p+1)/4)%(self.p - 1))
+		self.ramdom_shares[i] = r * v
+		self.triggers[i].callback(1)
 		
-		open_1.addCallback(self.plainprint)
-	'''
+	def preprocess_ready(self,result):
+		print "preprocess_ready"
+		record_stop()
+		self.write_to_file(self.ramdom_shares)
+		record_stop()
+		results = self.runtime.synchronize()
+		self.runtime.schedule_callback(results, lambda _: self.runtime.shutdown())
 
-    def write_to_file(self,shares):
-	#print "here"
-	filename = "party" + str(self.runtime.id) + "_butterfly_random_share"
-	
-	FD = open(filename, "w")
+		'''
+		for i in range(self.k * int(math.log(self.k,2))): 
+			open_1= self.runtime.open(self.ramdom_shares[i])
+			
+			open_1.addCallback(self.plainprint)
+		'''
 
-	content = str(self.k) + "\n" + str(self.p) + "\n"
-	for share in shares:
-		content = content + str(share.result)[1:-1] + "\n"
-	FD.write(content)
-	FD.close()
+	def write_to_file(self,shares):
+		#print "here"
+		filename = "party" + str(self.runtime.id) + "_butterfly_random_share"
+		
+		FD = open(filename, "w")
 
-    def __generate_initial_random_values(self, field, b):
-        return [rand.randint(0, field.modulus - 1) for _ in range(b)]
+		content = str(self.k) + "\n" + str(self.p) + "\n"
+		for share in shares:
+			content = content + str(share.result)[1:-1] + "\n"
+		FD.write(content)
+		FD.close()
 
-    def plainprint(self,result):
+	def __generate_initial_random_values(self, field, b):
+		return [rand.randint(0, field.modulus - 1) for _ in range(b)]
 
-	print result
+	def plainprint(self,result):
+
+		print result
 
 def errorHandler(failure):
-    print("Error: %s" % failure)
+	print("Error: %s" % failure)
 
 # Parse command line arguments.
 parser = OptionParser()
@@ -163,9 +162,9 @@ Toft05Runtime.add_options(parser)
 options, args = parser.parse_args()
 
 if len(args) == 0:
-    parser.error("you must specify a config file")
+	parser.error("you must specify a config file")
 else:
-    id, players = load_config(args[0])
+	id, players = load_config(args[0])
 k = int(sys.argv[3])
 # Create a deferred Runtime and ask it to run our protocol when ready.
 runtime_class = make_runtime_class(runtime_class=BasicActiveRuntime,
