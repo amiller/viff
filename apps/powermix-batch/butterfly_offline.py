9d755793412e69626fc1aaf3061e7b56fb6c9c79
Donghang Lu <lu562@purdue.edu>
2018-08-16 15:32:25 -0400

    fix

--- apps/powermix-batch/butterfly_offline.py
+++ apps/powermix-batch/butterfly_offline.py
@@ -1,33 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright 2007, 2008 VIFF Development Team.
-#
-# This file is part of VIFF, the Virtual Ideal Functionality Framework.
-#
-# VIFF is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License (LGPL) as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# VIFF is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
-# Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with VIFF. If not, see <http://www.gnu.org/licenses/>.
-
-# This example is a tribute to the original example of secure
-# multi-party computation by Yao in 1982. In his example two
-# millionaires meet in the street and they want to securely compare
-# their fortunes. They want to do so without revealing how much they
-# own to each other. This problem would be easy to solve if both
-# millionaires trust a common third party, but we want to solve it
-# without access to a third party.
-#
-# In this example the protocol is run between three millionaires and
-# uses a protocol for secure integer comparison by Toft from 2005.
-#
 # Give a player configuration file as a command line argument or run
 # the example with '--help' for help with the command line options.
 
@@ -41,14 +13,9 @@ from random import randint
 import time
 import math
 from viff.runtime import create_runtime, gather_shares,Share,make_runtime_class
-from viff.comparison import Toft05Runtime
 from viff.config import load_config
 from viff.util import rand, find_prime
 from viff.active import BasicActiveRuntime, TriplesHyperinvertibleMatricesMixin
-
-import sys
-
-
 
 import sys
 sys.setrecursionlimit(2000000)
@@ -66,7 +33,7 @@ def record_start():
 
 
 def record_stop():
-
+    global start
     stop = time.time()
     print
     print "Total time used: %.3f sec" % (stop-start)
@@ -77,22 +44,31 @@ def record_stop():
         f.close()
     '''
     print "*" * 64
+    start = stop
     #return x
 
+# MNT224
+#prime = 15028799613985034465755506450771561352583254744125520639296541195021L
+# JubJub
+#prime = 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001L
+prime = find_prime(2**256, blum=True)
+Zp = GF(prime)
 
+# TODO: implement general case of sqrt
+#def cipolla(v):
+# pass
 
 
 class OfflineProtocol:
     # this is only for testing generating random -1/1 shares.
-    def __init__(self, runtime,k):
-	self.k =k
+    def __init__(self, runtime, k):
+	self.k = k
 	self.runtime = runtime
-	self.ramdom_shares = [0 for i in range(self.k * int(math.log(self.k,2)))]
-	self.p = find_prime(2**256, blum=True)
-	self.Zp = GF(self.p)
-	self.triggers = [Share(self.runtime,self.Zp) for i in range(self.k * int(math.log(self.k,2)))]
-	
-	#print self.p
+	self.random_shares = [0 for i in range(self.k * int(math.log(self.k,2)))]
+	self.triggers = [Share(self.runtime,Zp) for i in range(self.k * int(math.log(self.k,2)))]
+	self.p = prime
+	print self.p
+        assert self.p % 4 == 3, "Need efficient square roots"
 
 	n = self.runtime.num_players
 	t = self.runtime.threshold
@@ -113,30 +89,41 @@ class OfflineProtocol:
 	list = [self.triggers[i] for i in range(self.k * int(math.log(self.k,2)))]
 	result = gather_shares(list)
 	result.addCallback(self.preprocess_ready)
+        runtime.schedule_callback(result, lambda _: runtime.synchronize())
+        runtime.schedule_callback(result, lambda _: runtime.shutdown())
 
+ 
+	
+        #data = [1,2,3,4,5,6,7,8]
+	#result = permutation_component(data,8)
+	#print result
 
     def calculate_share(self,result,r,i):
 	#print ""caculating shares
 	v = result**((-(self.p+1)/4)%(self.p - 1))
-	self.ramdom_shares[i] = r * v
+        
+	self.random_shares[i] = r * v
 	self.triggers[i].callback(1)
 	
     def preprocess_ready(self,result):
 	print "preprocess_ready"
 	record_stop()
-	self.write_to_file(self.ramdom_shares)
+	print "write to file"
+	self.write_to_file(self.random_shares)
+	print "finish writting"
 	record_stop()
-	results = self.runtime.synchronize()
-	self.runtime.schedule_callback(results, lambda _: self.runtime.shutdown())
+	self.runtime.print_transferred_data()
+        #results = self.runtime.synchronize()
+        #self.runtime.schedule_callback(results, lambda _: self.runtime.shutdown())
 
 	'''
 	for i in range(self.k * int(math.log(self.k,2))): 
-		open_1= self.runtime.open(self.ramdom_shares[i])
+		open_1= self.runtime.open(self.random_shares[i])
 		
 		open_1.addCallback(self.plainprint)
 	'''
 
-    def write_to_file(self,shares):
+    def write_to_file(self, shares):
 	#print "here"
 	filename = "party" + str(self.runtime.id) + "_butterfly_random_share"
 	
@@ -160,14 +147,14 @@ def errorHandler(failure):
 
 # Parse command line arguments.
 parser = OptionParser()
-Toft05Runtime.add_options(parser)
+BasicActiveRuntime.add_options(parser)
 options, args = parser.parse_args()
 
 if len(args) == 0:
     parser.error("you must specify a config file")
 else:
     id, players = load_config(args[0])
-k = int(args[1])
+k = int(sys.argv[3])
 # Create a deferred Runtime and ask it to run our protocol when ready.
 runtime_class = make_runtime_class(runtime_class=BasicActiveRuntime,
     mixins=[TriplesHyperinvertibleMatricesMixin])
